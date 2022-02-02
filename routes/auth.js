ad7207dc728a2bbbffaf7e7370d6d8301494d0b0
Priyanka1818 <77811257+Priyanka1818@users.noreply.github.com>
2022-02-01 21:43:36 -0600

    Update auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -4,24 +4,12 @@ var passport = require('passport');
 var dotenv = require('dotenv');
 var util = require('util');
 var url = require('url');
-var querystring = require('querystring');
-
-
-
-dotenv.config();
-
-
-
-// Perform the login, after login Auth0 will redirect to callback
+var querystring = require('querystring');dotenv.config();// Perform the login, after login Auth0 will redirect to callback
 router.get('/login', passport.authenticate('auth0', {
 scope: 'openid email profile'
 }), function (req, res) {
 res.redirect('/');
-});
-
-
-
-// Perform the final stage of authentication and redirect to previously requested URL or '/user'
+});// Perform the final stage of authentication and redirect to previously requested URL or '/user'
 router.get('/callback', function (req, res, next) {
 passport.authenticate('auth0', function (err, user, info) {
 if (err) { return next(err); }
@@ -33,36 +21,20 @@ delete req.session.returnTo;
 res.redirect(returnTo || '/user');
 });
 })(req, res, next);
-});
-
-
-
-// Perform session logout and redirect to homepage
+});// Perform session logout and redirect to homepage
 router.get('/logout', (req, res) => {
 req.logout();
 //https://community.auth0.com/t/having-trouble-with-logout-on-the-sample-nodejs-app/18172/10
 if (req.session) {
 req.session.destroy(function (err) {
 if (err) { console.log(err) }
-console.log("Destroyed the user session on Auth0 endpoint");
-
-
-
-var returnTo = req.protocol + '://' + req.hostname;
+console.log("Destroyed the user session on Auth0 endpoint"); var returnTo = req.protocol + '://' + req.hostname;
 var port = req.connection.localPort;
-if (port !== undefined && port !== 80 && port !== 443) {
-
-
-
-// https://auth0.com/blog/create-a-simple-and-secure-node-express-app/
+if (port !== undefined && port !== 80 && port !== 443) { // https://auth0.com/blog/create-a-simple-and-secure-node-express-app/
 returnTo =
 process.env.NODE_ENV === "production"
 ? `${returnTo}/`
-: `${returnTo}:${port}`;
-
-
-
-}
+: `${returnTo}:${port}`; }
 var logoutURL = new url.URL(
 util.format('https://%s/v2/logout', process.env.AUTH0_DOMAIN)
 );
@@ -76,8 +48,4 @@ res.redirect(logoutURL);
 // https:// auth0.com/v2/logout?client_id=ffffffffff&returnTo=https%3A%2F%2Ftest-auth0-case.herokuapp.com%3A43403
 });
 }
-});
-
-
-
-module.exports = router;
+});module.exports = router;
